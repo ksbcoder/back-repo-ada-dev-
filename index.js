851019f9ff94fa75868f7f95ad459d77d7f2cb21
Kevin <baquerochavarro@gmail.com>
2021-12-17 19:53:28 -0500

    agregar cors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import { ApolloServer } from 'apollo-server-express';
-import dotenv from 'dotenv';
-import conectarBD from './db/db.js';
-import { tipos } from './graphql/types.js';
-import { resolvers } from './graphql/resolvers.js';
-import { validateToken } from './utils/tokenUtils.js';
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+import dotenv from "dotenv";
+import conectarBD from "./db/db.js";
+import { tipos } from "./graphql/types.js";
+import { resolvers } from "./graphql/resolvers.js";
+import { validateToken } from "./utils/tokenUtils.js";
 
 dotenv.config();
 
 const getUserData = (token) => {
-  const verificacion = validateToken(token.split(' ')[1]);
+  const verificacion = validateToken(token.split(" ")[1]);
   if (verificacion.data) {
     return verificacion.data;
   } else {
@@ -40,11 +40,20 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen({ port: process.env.PORT || 4000 }, async () => {
   await conectarBD();
   await server.start();
 
   server.applyMiddleware({ app });
 
-  console.log('servidor listo');
+  console.log("servidor listo");
 });
